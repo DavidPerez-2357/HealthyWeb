ec49adc64de44ec126887f4e6554b296c3246748
david <dperezfernandez@safareyes.es>
2024-09-19 12:03:49 +0200

    💄 Started the index page

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+import InfoCard from './components/InfoCard';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <InfoCard title="Mejora tu salud paso a paso">
+          Dar el primer paso hacia una vida más saludable puede parecer abrumador, pero no tiene que serlo.
+          En esta sección, te guiamos a través de un enfoque gradual y manejable para incorporar hábitos
+          saludables en tu rutina diaria. Desde simples ajustes en tu dieta hasta estrategias para mantener
+          un equilibrio entre la alimentación y el bienestar general, te proporcionamos consejos prácticos
+          y accesibles para que puedas hacer cambios sostenibles y positivos en tu vida. Aquí, descubrirás
+          cómo pequeños cambios pueden generar grandes beneficios y cómo cada paso cuenta en tu camino
+          hacia una salud óptima. ¡Comienza hoy y transforma tu bienestar de manera simple y efectiva!
+        </InfoCard>
     </>
   )
 }
