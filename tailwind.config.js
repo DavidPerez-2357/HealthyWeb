01d761b82d576d1f25a0b0aef201775b13244c5e
david <dperezfernandez@safareyes.es>
2024-09-19 12:02:56 +0200

    💄 Added custom styles and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-500': '#2CCE4C',
+        'primary-700': '#1C9D36',
+      },
+      fontFamily: {
+        sans: ['Lexend', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
