312c5b66bdd4647b7f2c93ce48fcfc0d5bb8b63e
DavidClase <dperezfernandez@safareyes.es>
2024-09-20 20:48:53 +0200

    💄 Changed styles

--- src/components/IndexHeader.jsx
+++ src/components/IndexHeader.jsx
@@ -2,9 +2,9 @@ import vegetables from '../assets/vegetables.jpg'
 
 const IndexHeader = ({ children }) => {
   return (
-      <header className={'h-[200px] flex justify-center items-center relative w-100 overflow-hidden bg-gradient-to-t rounded-b-xl from-primary-500 to-transparent'}>
+      <header className={'h-[200px] flex justify-center items-center relative w-100 overflow-hidden bg-gradient-to-t rounded-b-xl  shadow-gray-400 shadow-md from-primary-500 to-transparent'}>
         <img src={ vegetables } alt={'Vegetables'} className='absolute -z-10'/>
-        <h1 className='text-white font-bold text-6xl align-middle text-center'>{children}</h1>
+        <h1 className='text-white font-bold md:text-6xl text-5xl align-middle text-center'>{children}</h1>
       </header>
   )
 };
