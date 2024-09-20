aca27c2833ec2b1da0e5bcb6daf3d8a9b4d3c6c5
DavidClase <dperezfernandez@safareyes.es>
2024-09-20 20:48:24 +0200

    💄 Added shadow

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,6 +1,6 @@
 const InfoCard = ({ title, children, className = '' }) => {
     return (
-        <fieldset className={`bg-white border-2 border-primary-500 p-5 rounded-lg shadow-md ${className}`}>
+        <fieldset className={`bg-white border-2 shadow-gray-300 border-primary-500 p-5 rounded-lg shadow-md ${className}`}>
             <legend className="text-primary-700 text-xl font-bold text-left px-2 mb-0">{title}</legend>
             <p className="text-gray-600 leading-relaxed text-justify">
                 {children}
