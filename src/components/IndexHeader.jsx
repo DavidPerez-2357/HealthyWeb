c6c07ad4d0c38bdc265ca82ad3e33ca9be88f0c7
DavidClase <dperezfernandez@safareyes.es>
2024-09-23 20:41:18 +0200

    💄 Created class main_title

--- src/components/IndexHeader.jsx
+++ src/components/IndexHeader.jsx
@@ -4,7 +4,7 @@ const IndexHeader = ({ children }) => {
   return (
       <header className={'h-[200px] flex justify-center items-center relative w-100 overflow-hidden bg-gradient-to-t rounded-b-xl  shadow-gray-400 shadow-md from-primary-500 to-transparent'}>
         <img src={ vegetables } alt={'Vegetables'} className='absolute -z-10'/>
-        <h1 className='text-white font-bold md:text-6xl text-5xl align-middle text-center'>{children}</h1>
+        <h1 className='main_title text-white'>{children}</h1>
       </header>
   )
 };
