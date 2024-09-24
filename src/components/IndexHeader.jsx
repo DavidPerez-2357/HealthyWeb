00e1b25fc7c0b2945d8333bbbfa1e66ef0f3756b
DavidClase <dperezfernandez@safareyes.es>
2024-09-24 20:59:54 +0200

    🎨 Changed the component to a parent

--- src/components/IndexHeader.jsx
+++ src/components/IndexHeader.jsx
@@ -1,11 +1,9 @@
 import vegetables from '../assets/vegetables.jpg'
+import Header from "./Header.jsx";
 
 const IndexHeader = ({ children }) => {
   return (
-      <header className={'h-[200px] flex justify-center items-center relative w-100 overflow-hidden bg-gradient-to-t rounded-b-xl  shadow-gray-400 shadow-md from-primary-500 to-transparent'}>
-        <img src={ vegetables } alt={'Vegetables'} className='absolute -z-10'/>
-        <h1 className='main_title text-white'>{children}</h1>
-      </header>
+      <Header image={vegetables} bg_color='green'>{children}</Header>
   )
 };
 
