b42646aded6e29260c717cef73e38d402c0ae398
DavidClase <dperezfernandez@safareyes.es>
2024-09-26 18:00:04 +0200

    💄 Style the page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import RecipeHeader from "../components/RecipeHeader.jsx";
 import {getInformationRecipe} from "../services/recipeService.js";
 import Loading from "../components/Loading.jsx";
@@ -13,6 +13,7 @@ function Recipe () {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const search = new URLSearchParams(window.location.search).get('search');
 
     useEffect(() => {
         // Función asíncrona dentro de useEffect
@@ -46,11 +47,25 @@ function Recipe () {
             <RecipeHeader>{data.name}</RecipeHeader>
 
             <main className='main_content'>
-                <div className='md:w-1/2'>
+                <Link to={`/?search=${search}`}>
+                    <div className='text-gray-700 flex items-center gap-3 px-3 max-w-max mb-5 p-2 bg-gray-300 hover:bg-gray-700 hover:text-gray-200 transition rounded'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-house-door-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5"/>
+                        </svg>
+                        Back to recipes
+                    </div>
+                </Link>
+
+                <div className='w-full p-5 grid sm:gap-2 gap-6'>
                     <SectionTitle>Ingredients</SectionTitle>
-                    {Object.entries(data.ingredients).map(([type, ingredients]) => (
-                        <IngredientTypeGroup key={type} type={type} ingredients={ingredients}/>
-                    ))}
+                    <div className='sm:grid flex flex-wrap gap-x-20 gap-y-6'
+                         style={{gridTemplateColumns: 'repeat(auto-fit, minmax(500px, 1fr))'}}>
+                        {Object.entries(data.ingredients).map(([type, ingredients]) => (
+                            <IngredientTypeGroup key={type} type={type} ingredients={ingredients}/>
+                        ))}
+                    </div>
                 </div>
             </main>
         </>
