7f2c9e9501205988c014bb798c47bb8d22ccf68e
DavidClase <dperezfernandez@safareyes.es>
2024-09-26 17:58:57 +0200

    ♻️ Added a new attribute

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,8 @@
 import apple_crying from '/src/assets/apple_crying.gif';
 
-const Error = ({ title='Error', message }) => {
+const Error = ({ title='Error', message, className }) => {
     return (
-        <div className="flex justify-center items-center flex-col" style={{height: '100vh'}}>
+        <div className={`flex justify-center items-center flex-col ${className}`} style={{height: '100vh'}}>
             <img src={ apple_crying } className="w-52" alt="Broccoli dancing" />
             <h1 className="main_title text-primary-500">{title}</h1>
             <p className="text-center text-gray-500 mt-2">{message}</p>
