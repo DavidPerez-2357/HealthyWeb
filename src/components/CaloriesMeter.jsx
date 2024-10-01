d9da6e63e1c13e7596bd21d2b6122961b9f455aa
david <dperezfernandez@safareyes.es>
2024-10-01 12:52:45 +0200

    ✨ Add alternating meter depends in total or per portion

--- src/components/CaloriesMeter.jsx
+++ src/components/CaloriesMeter.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 
-const CaloriesMeter = ({ calories }) => {
-    const levels = {
-        low: 300,
-        medium: 600,
-        high: 900,
-        very_high: 1200,
+const CaloriesMeter = ({ calories, type }) => {
+    let levels = {
+        low: 0,
+        medium: 0,
+        high: 0,
+        very_high: 0,
     };
+
+    const levelsPerPortion = levels;
+    levelsPerPortion.low = 300;
+    levelsPerPortion.medium = 600;
+    levelsPerPortion.high = 900;
+    levelsPerPortion.very_high = 1200;
+
+    const levelsPerTotal = levels;
+    levelsPerTotal.low = 1000;
+    levelsPerTotal.medium = 2000;
+    levelsPerTotal.high = 3000;
+    levelsPerTotal.very_high = 4000;
+
+    if (type === 'portion') {
+        levels = levelsPerPortion;
+    }else {
+        levels = levelsPerTotal;
+    }
 
     let percentage = (calories / levels.very_high) * 100;
 
@@ -15,11 +33,42 @@ const CaloriesMeter = ({ calories }) => {
     }
 
     return (
-        <div className={'w-full max-w-[800px] h-5 pt-0 grid relative grid-cols-4 gap-2 '}>
-            <div className='bg-green-500 rounded-l h-5'></div>
-            <div className='bg-orange-500 orange h-5'></div>
-            <div className='bg-red-500 h-5'></div>
-            <div className='bg-gray-800 rounded-r h-5'></div>
+        <div className={'w-full max-w-[800px] h-5 pt-0 grid relative grid-cols-4 gap-2'}>
+            <div className='relative group bg-green-500 rounded-l h-5 hover:scale-105 transition'
+                 title={`0kcal - ${levels.low}kcal`}>
+                {/* Texto Low */}
+                <div
+                    className='absolute top-full mt-2 text-gray-700 sm:flex hidden w-full justify-center group-hover:font-bold'>
+                    Low
+                </div>
+            </div>
+
+            <div className='relative group bg-orange-500 h-5 hover:scale-105 transition'
+                 title={`${levels.low}kcal - ${levels.medium}kcal`}>
+                {/* Texto Medium */}
+                <div
+                    className='absolute top-full mt-2 text-gray-700 sm:flex hidden w-full justify-center group-hover:font-bold'>
+                    Medium
+                </div>
+            </div>
+
+            <div className='relative group bg-red-500 h-5 hover:scale-105 transition'
+                 title={`${levels.medium}kcal - ${levels.high}kcal`}>
+                {/* Texto High */}
+                <div
+                    className='absolute top-full mt-2 text-gray-700 sm:flex hidden w-full justify-center group-hover:font-bold'>
+                    High
+                </div>
+            </div>
+
+            <div className='relative group bg-gray-800 rounded-r h-5 hover:scale-105 transition'
+                 title={`+ ${levels.high}kcal`}>
+                {/* Texto Very High */}
+                <div
+                    className='absolute top-full mt-2 text-gray-700 sm:flex hidden w-full justify-center group-hover:font-bold'>
+                    Very High
+                </div>
+            </div>
 
             <div className='absolute w-1 h-7 translate -translate-y-1/2 top-1/2 bg-gray-700'
                  style={{left: `${percentage}%`}}></div>
@@ -29,12 +78,8 @@ const CaloriesMeter = ({ calories }) => {
                 style={{left: `${percentage}%`}}>
                 {calories}
             </div>
-
-            <div className='text-gray-700 sm:flex hidden w-full justify-center'>Low</div>
-            <div className='text-gray-700 sm:flex hidden w-full justify-center'>Medium</div>
-            <div className='text-gray-700 sm:flex hidden w-full justify-center'>High</div>
-            <div className='text-gray-700 sm:flex hidden w-full justify-center'>Very high</div>
         </div>
+
 
     );
 }
